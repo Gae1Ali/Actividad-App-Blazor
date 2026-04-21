3b697b560bff3b8c42c7d65c27a9297371e1e2b7
Gael Briseño <gbriseno@ucol.mx>
2026-04-20 21:46:54 -0600

    Actividad 16

---
+++ Actividad App Blazor/Components/Pages/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Estudiante
+{
+    [Required(ErrorMessage = "El nombre no debe ser vacío")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "El correo no debe ser vacío")]
+    [EmailAddress(ErrorMessage = "Correo no válido")]
+    public string Correo { get; set; }
+
+    [Range(0, 10, ErrorMessage = "La calificación debe estar entre 0 y 10")]
+    public double Calificacion { get; set; }
+}
